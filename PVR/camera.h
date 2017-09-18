f61ce1b4568c062b970135c49a452598ba7cdfda
penguin <penguin1214@users.noreply.github.com>
2017-09-18 14:31:12 +0800

    fix camera

--- PVR/camera.h
+++ PVR/camera.h
@@ -3,6 +3,9 @@
 
 #include "Vector3.h"
 
+#define _USE_MATH_DEFINES
+#include <math.h>
+
 class Ray {
 public:
 	Vector3 origin;
@@ -14,33 +17,38 @@ public:
 class Film {
 public:
 	Film() {}
-	Film(double wid, double hei) :_w(wid), _h(hei) {}
+	Film(int wid, int hei) :_w(wid), _h(hei) {}
 
-	double _w, _h;	// film size
+	int _w, _h;	// film size
 	double _vfov;
 	double _nearPlaneDistance;
 
 	double aspectRatio() { return _w / _h; }
-	void setFilm(double w, double h, double vfov, double dis) {
+	void setFilm(int w, int h, double vfov, double dis) {
 		_w = w; _h = h; _vfov = vfov; _nearPlaneDistance = dis;
 	}
 	void setFovAndDis(double fov, double dis, double aspr) {
 		_vfov = fov; _nearPlaneDistance = dis;
 		_h = 2 * _nearPlaneDistance*tan(fov / 2);
 		_w = _h * aspr;
+		std::cout << "w: " << _w << ", h: " << _h << std::endl;
+		std::cout << "film vfov(in degree): " << _vfov * 180 / M_PI << ", distance: " << _nearPlaneDistance << std::endl;
 	}
+
 	void setSize(int w, int h) {
 		_w = w; _h = h;
 	}
+
 	void setFov(double fov) {
 		_vfov = fov;
 		_nearPlaneDistance = _h / (2 * tan(fov / 2));
-		std::cout << "film vfov: " << _vfov << ", distance: " << _nearPlaneDistance << std::endl;
+		std::cout << "film vfov(in degree): " << _vfov * 180 / M_PI << ", distance: " << _nearPlaneDistance << std::endl;
 	}
+
 	void setDis(double dis) {
 		_nearPlaneDistance = dis;
 		_vfov = 2 * atan(_h / (2 * _nearPlaneDistance));
-		std::cout << "film vfov: " << _vfov << ", distance: " << _nearPlaneDistance << std::endl;
+		std::cout << "film vfov(in degree): " << _vfov * 180 / M_PI << ", distance: " << _nearPlaneDistance << std::endl;
 	}
 };
 
